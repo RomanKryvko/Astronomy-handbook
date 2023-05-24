fc5b24849ffaae1eea7a39b6619c6c088a78a5ec
Roman Kryvko <roman.kr43@gmail.com>
2023-05-24 03:06:03 +0200

    Form updated.

---
+++ Star.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Довідник_астронома
+{
+    internal class Star
+    {
+        public string name;
+        string constellation;
+        double apparentMagnitude;
+        double distance;
+        //RA stored in seconds.
+        double rightAscension;
+        //Dec stred in seconds.
+        double declination;
+    }
+}
